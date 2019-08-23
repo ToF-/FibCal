20fe0c95cb0fa39b74e845a7de306a3025c2181b
Christophe Thibaut <cthibaut@octo.com>
2019-08-23 08:20:42 +0200

    wrote code in the red

--- FibCal.hs
+++ FibCal.hs
@@ -5,6 +5,11 @@ import Data.Char
 data Color = Black | Purple | Green | Blue | Red | Orange
     deriving (Show)
 
+type Direction = (Integer, Integer)
+
+type Coord = (Integer, Integer)
+
+
 fibCal :: Show a => [a] -> [Color] -> [String]
 fibCal xs cs = map showNode (zip3 xs cs [(0,0),(0,1)])
     where
@@ -14,6 +19,28 @@ fibCal xs cs = map showNode (zip3 xs cs [(0,0),(0,1)])
         ++ ",minimum size=0.9cm,inner sep=0pt] at "
         ++ show coords
         ++" {$"++ show n ++"$};"
+
+squares :: [Coord]
+squares = coords $ foldl nextSquare initial $ fibs
+
+coords (c,d,cs) = cs
+
+initial :: (Coord, Direction, [Coord]) 
+initial = ((-1,-1),(1,1),[])
+
+nextSquare :: (Coord, Direction, [Coord]) -> Integer -> (Coord, Direction, [Coord])
+nextSquare ((x,y),(dx,dy),s) n = ((x+dx*n,y+dy*n), next (dx,dy), s ++ square n (x,y) (dx,dy))
+
+square :: Integer -> Direction -> Coord -> [Coord]
+square n (dx,dy) (x,y) = [(i,j) | j <- range y dy y, i <- range y dx x]
+
+range a d n = map (\i-> a + i * d) [0..n-1]
+
+next :: Direction -> Direction
+next (1,1) = (-1,-1)
+next (-1,-1) = (1,1)
+
+fibs = [1,1,2,3,5,8,13]
 
 
 -- [1,1,2,3,5,8,13,21,..]
